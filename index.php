73c9d566a5664ce2c3861a5a4448217affcb6813
elvinmammadoff <elvinmammadoff@gmail.com>
2018-03-07 00:34:51 +0400

    added title into head

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Meta tags -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Title</title>
+    <title>Buta Pizza - Pizza / Restaurant / Coffee / Cafe</title>
 
     <!-- Stylesheets -->
     <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" media="all" />
